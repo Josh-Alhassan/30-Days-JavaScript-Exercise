ab10e95f09e489b79cfa92dbbde4d9f6a294dd45
Saint-Josh <saintjosh26@gmail.com>
2022-08-20 15:15:11 +0100

    question 6

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+import './index.js';
+// Question 5: Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let stringValue = 'This is a string value';
+let booleanValue = true;
+let undfinedValue = undefined;
+let nullValue = null;
+
+console.log(typeof stringValue);
+console.log(typeof booleanValue);
+console.log(typeof undfinedValue);
+console.log(typeof nullValue);
+
+// Question 6: Declare four variables without assigning values
+let varOne;
+let varTwo;
+let varThree;
+let varFour;
