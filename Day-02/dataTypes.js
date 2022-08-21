e271cd8fc33bc704379062d7fc7aafc50fd4113d
Saint-Josh <saintjosh26@gmail.com>
2022-08-22 00:29:39 +0100

    non-primitive data type

--- Day-02/dataTypes.js
+++ Day-02/dataTypes.js
@@ -1,7 +1,7 @@
 import '../index.js';
 
-console.log(' ')
-console.log('#########################')
+console.log(' ');
+console.log('#########################');
 console.log('Hello Day 2');
 console.log('#########################');
 
@@ -9,18 +9,16 @@ console.log('#########################');
   Primitive Data types are immutable (non-modifiable)
 */
 let word = 'JavaScript';
-console.log(word)
+console.log(word);
 
 // Throws error
 // word[0] = 'Y';
 console.log(word);
 
-/*
-  Primitive Data types are compared by its values
-*/
+// Primitive Data types are compared by its values
 let numOne = 3;
 let numTwo = 3;
-console.log(numOne == numTwo)
+console.log(numOne == numTwo);
 
 // Next Check
 let js = 'JavaScript';
@@ -31,3 +29,34 @@ console.log(js == py);
 let lightOn = true;
 let lightOff = false;
 console.log(lightOn == lightOff);
+
+/*
+  Non-Primitive Data Types are mutable or modifiable. They cannot be compared by value
+*/
+
+// Arrays are mutable
+let nums = [1, 2, 3];
+nums[0] = 10;
+console.log(nums);
+
+// ** Rule of Thumb Non-primitive data types (functions, array, or Objects) cannot be compared by value
+let numArrOne = [1, 2, 3];
+let numArrTwo = [1, 2, 3];
+console.log(numArrOne == numArrTwo); //False
+
+let userOne = {
+  name: 'Joshua',
+  role: 'Student',
+  country: 'Nigeria',
+};
+
+let userTwo = {
+  name: 'Joshua',
+  role: 'Student',
+  country: 'Nigeria',
+};
+console.log(userOne == userTwo); // false
+
+// Two object are only strictly equal if they refer to the same underlying object
+userTwo = userOne;
+console.log(userOne == userTwo); // true
